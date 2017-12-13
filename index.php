bff680677e5b996050d5ba974943e3967f8caa94
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 10:54:53 +0100

    ajout exo8

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>exercice 8 partie 3 php</title>
-  </head>
-  <body>
-    <?php
-    $text = 'Enfin';
-      for ($number = 200; $number >= 0; $number-=12) {
-        echo $text.'<br/>';
-      }
-     ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>exercice 7 partie 7 php</title>
+</head>
+<body>
+  <p><?php
+  if (isset($_POST['civility']) && ($_POST['lastname']) && ($_POST['firstname']) && isset($_FILES['myFile'])) {
+    $infosfichier = pathinfo($_FILES['myFile']['name']);
+    $extension_upload = $infosfichier['extension'];
+    $extensions_autorisees = array('pdf');
+    if (in_array($extension_upload, $extensions_autorisees))
+    {
+      echo 'Bonjour ' . $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'] . '. Votre fichier est : ' . $_FILES['myFile']['name'];
+      $infosfichier = pathinfo($_FILES['myFile']['name']);
+    } else {
+      echo 'Ce fichier n\'est pas un fichier PDF !';
+    }
+  } else {
+    ?>
+    <form class="form" action="index.php" method="post" enctype="multipart/form-data">
+      <select class="civility" name="civility">
+        <option value="Monsieur" name="Monsieur">Monsieur</option>
+        <option value="Madame" name="Madame">Madame</option>
+      </select>
+      <input type="lastname" name="lastname" placeholder="Nom">
+      <input type="firstname" name="firstname" placeholder="Prénom">
+      <input type="file" name="myFile">
+      <button type="submit" name="validate">Valider</button>
+    </form>
+  <?php }
+  ?>
+</p>
+</body>
 </html>
